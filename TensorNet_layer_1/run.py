081fb82a2316052a3ccc5a202ac79d08a9503b76
Katy <katyprogrammer@gmail.com>
2016-07-22 01:53:19 +0800

    modify run.py

--- TensorNet_layer_1/run.py
+++ TensorNet_layer_1/run.py
@@ -13,8 +13,8 @@ def B():
 # runB.start()
 # runB.join()
 
-def runR(r):
-    subprocess.call('python 20newsgroup.py -r B -l A.pkl -d B_{0}.pkl -e 10000 -R {0} > B_{0}.txt'.format(r), shell=True)
+def runR(r, i):
+    subprocess.call('python 20newsgroup.py -r B -l A.pkl -d B_{0}.pkl -e 10000 -R {0} > B_{0}_{1}.txt'.format(r, i), shell=True)
 def runRevR(r):
     subprocess.call('python 20newsgroup.py -r A -l B.pkl -d A_{0}.pkl -e 10000 -R {0} > A_{0}.txt'.format(r), shell=True)
 
@@ -23,10 +23,11 @@ tn = 0
 # all layers
 R = [1,5,10,50,100,200]
 tn = 0
-for r in R:
-    AB = mp.Process(target=runR, args=(r,))
-    AB.start()
-    AB.join()
-    # BA = mp.Process(target=runRevR, args=(r,))
-    # BA.start()
-    # BA.join()
+for i in range(5):
+    for r in R:
+        AB = mp.Process(target=runR, args=(r,i))
+        AB.start()
+        AB.join()
+        # BA = mp.Process(target=runRevR, args=(r,))
+        # BA.start()
+        # BA.join()
